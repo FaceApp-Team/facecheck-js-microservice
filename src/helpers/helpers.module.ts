c8b0ce055289ca34c26802f85855cf4fe9ddd8ee
Dickson Daniel Peprah <techpepson@gmail.com>
2026-01-23 02:25:02 +0000

    update(source of email): update the source of email file template to point to right source

--- src/helpers/helpers.module.ts
+++ src/helpers/helpers.module.ts
@@ -17,7 +17,7 @@ import { HttpModule } from '@nestjs/axios';
           from: '"College of Medicine and Allied Sciences" <info@comas.edu.gh>',
         },
         template: {
-          dir: join(__dirname, '..', 'views'),
+          dir: join(__dirname, '..', '..', 'views'),
           adapter: new EjsAdapter(),
         },
       }),
